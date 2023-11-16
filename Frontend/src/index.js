a9428c0ff98f915782fdca84dce2b669eead2999
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2023-11-16 20:52:37 +0100

    WIP

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from './App';
+import ForumMain from "./Forum/ForumMain.js"
 import './index.css'
+
+export default function AppUrls(){
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}/>
+          <Route path='/forum' element={<ForumMain/>}/>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppUrls/>
   </React.StrictMode>
 );
