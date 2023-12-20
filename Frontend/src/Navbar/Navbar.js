3e4a9bc3d1ad218b7146d9d949af48ed2c36b160
CampoNover16 <s.mackiewicz00@gmail.com>
2023-12-20 01:39:02 +0100

    added early version of map filters(not working bcs lack of point on map) + fixed some css styles

--- Frontend/src/Navbar/Navbar.js
+++ Frontend/src/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { Outlet, Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import DropdownItem from './DropdownItem/DropdownItem';
 import SearchBar from './SearchBar/SearchBar';
+import MapFilter from './MapFilters/Mapfilter';
 
 function Navbar() {
   const [isActive, setIsActive] = useState(false);
@@ -13,37 +14,42 @@ function Navbar() {
 
   return (
     <>
-      <nav className='absolute top-4 left-4 flex gap-2 items-center'>
-        <div>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            className='rounded-full border-2 h-12 w-12 border-black flex justify-center items-center cursor-pointer bg-slate-300'
-            onClick={handleClick}
-          >
-            <img
-              src={`./assets/${!isActive ? 'menu_icon' : 'cancel_icon'}.svg`}
-              alt='menu_icon'
-              className='h-8 w-8'
-            ></img>
-          </motion.div>
-          {isActive && (
+      <nav className='relative flex justify-between p-3'>
+        <div className='flex gap-2 items-center'>
+          <div>
             <motion.div
-              exit={{ opacity: 0 }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className='flex flex-col gap-2 mt-2 absolute'
+              whileHover={{ scale: 1.1 }}
+              className='rounded-full border-2 h-12 w-12 border-black flex justify-center items-center cursor-pointer bg-slate-300'
+              onClick={handleClick}
             >
-              <Link to='/'>
-                <DropdownItem icon='map_icon' name='Main page'></DropdownItem>
-              </Link>
-              <Link to='/forum'>
-                <DropdownItem icon='forum_icon' name='Forum'></DropdownItem>
-              </Link>
-              <DropdownItem icon='forum_icon'></DropdownItem>
+              <img
+                src={`./assets/${!isActive ? 'menu_icon' : 'cancel_icon'}.svg`}
+                alt='menu_icon'
+                className='h-8 w-8'
+              ></img>
             </motion.div>
-          )}
+            {isActive && (
+              <motion.div
+                exit={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className='flex flex-col gap-2 mt-2 absolute'
+              >
+                <Link to='/'>
+                  <DropdownItem icon='map_icon' name='Main page'></DropdownItem>
+                </Link>
+                <Link to='/forum'>
+                  <DropdownItem icon='forum_icon' name='Forum'></DropdownItem>
+                </Link>
+                <DropdownItem icon='forum_icon'></DropdownItem>
+              </motion.div>
+            )}
+          </div>
+          <SearchBar />
         </div>
-        <SearchBar />
+        <div>
+          <MapFilter />
+        </div>
       </nav>
 
       <Outlet />
