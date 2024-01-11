486e3f189ec7ebc3ded15610b90acd26371c5ae4
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2024-01-11 22:14:08 +0100

    Fix and add new fields to Post
    Now we need to use headers when we use POST method after changes.

--- Frontend/src/Forum/PostForm.jsx
+++ Frontend/src/Forum/PostForm.jsx
@@ -7,9 +7,15 @@ function PostForm() {
 
   const handleSubmit = () => {
     const item = { title: title, content: content, author_id: 1, subforum: 1 };
-    axios.post('http://127.0.0.1:8000/memo_places_forum/posts/', item).catch((error) => {
-      console.log('Erorr with :', error);
-    });
+    axios
+      .post('http://127.0.0.1:8000/memo_places_forum/posts/', item, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .catch((error) => {
+        console.log('Erorr with :', error);
+      });
     console.log(`${title},${content}`);
   };
 
