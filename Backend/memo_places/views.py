7fad88a44669a974e53134591d572f7339f66c07
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2024-01-18 20:54:27 +0100

    User menagment
    I seperate options to reset password and change data like email etc.
    DELETE method don't realy delete user only changes to disactive status.

--- Backend/memo_places/views.py
+++ Backend/memo_places/views.py
@@ -38,6 +38,18 @@ class Place_view(viewsets.ModelViewSet):
                 places = Place.objects.filter(user=value)
                 serializer = Places_serailizer(places, many=True)
                 return Response(serializer.data)  
+            case "type":
+                places = Place.objects.filter(type=value)
+                serializer = Places_serailizer(places, many=True)
+                return Response(serializer.data)  
+            case "sortof":
+                places = Place.objects.filter(sortof=value)
+                serializer = Places_serailizer(places, many=True)
+                return Response(serializer.data)  
+            case "period":
+                places = Place.objects.filter(period=value)
+                serializer = Places_serailizer(places, many=True)
+                return Response(serializer.data)  
             case _:
                 place = None
                 return Response({'detail': 'Invalid key'})
@@ -71,7 +83,41 @@ class Place_view(viewsets.ModelViewSet):
         return Response(serializer.data)
 class User_view(viewsets.ModelViewSet):
     serializer_class =User_serializer
-    http_method_names = ['post']
-
+    # http_method_names = ['post','put','delete']
+ 
     def get_queryset(self):
-        return User.objects.none()
+        return User.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        # user_object = User.objects.get(id=kwargs['pk'])
+        try:
+            print(isinstance(int(kwargs['pk']),int))
+            user_object = User.objects.get(id=kwargs['pk'])
+        except:
+            key, value = re.match("(\w+)=(\d+)", kwargs['pk']).groups()
+            user_object = User.objects.get(id=value)
+            
+        data = request.data
+        if key == "password_reset":
+            user_object.set_password(data['password'])
+        else:
+            user_object.email = data["email"]
+            user_object.username = data["username"]
+            user_object.full_name = data["full_name"]
+            user_object.phone = data["phone"]
+            user_object.fb_link = data["fb_link"]
+
+        user_object.save()
+
+        serializer = User_serializer(user_object)
+        return Response(serializer.data)
+
+    def destroy(self, request,*args, **kwargs):
+        user_object = User.objects.get(id=kwargs['pk'])
+
+        user_object.active=False
+
+        user_object.save()
+
+        serializer = User_serializer(user_object)
+        return Response(serializer.data)
