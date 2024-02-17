64f375c12c21f8a975aee9d7637e514d305b1ca8
Mikolajkusinski <mikolajk558@gmail.com>
2024-02-17 08:50:35 +0100

    Need to pull Sebastian changes

--- Frontend/src/User/UserPlacesList.jsx
+++ Frontend/src/User/UserPlacesList.jsx
@@ -1,27 +1,16 @@
-import { useEffect } from 'react';
-import { selectUpdatePlace } from '../Redux/updatePlaceSlice';
+import { useState } from 'react';
 import UserPlaceItem from './UserPlaceItem';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
 
-const UserPlacesList = () => {
-  const updatePlaceData = useSelector(selectUpdatePlace);
-  const user = sessionStorage.getItem('user');
-  let userPlaces;
-  useEffect(() => {
-    axios.get(`http://localhost:8000/memo_places/places/user=${user.id}`).then((data) => {
-      userPlaces = data;
-    });
-  }, []);
-
-  useEffect(() => {
-    axios.get(`http://localhost:8000/memo_places/places/user=${user.id}`).then((data) => {
-      userPlaces = data;
-    });
-  }, [updatePlaceData]);
+const UserPlacesList = ({ userPlaces }) => {
+  const [clicked, setClicked] = useState(null);
 
   const placesList = Object.keys(userPlaces).map((place, key) => (
-    <UserPlaceItem key={key} place={userPlaces[place]} />
+    <UserPlaceItem
+      key={key}
+      place={userPlaces[place]}
+      clickedItem={clicked}
+      setClicked={setClicked}
+    />
   ));
 
   return <ul className='w-full h-full overflow-scroll '>{placesList}</ul>;
