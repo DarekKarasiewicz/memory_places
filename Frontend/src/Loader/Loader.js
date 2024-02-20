c0ad18653cb19bb21a8137709326b7444cda44ab
CampoNover16 <s.mackiewicz00@gmail.com>
2024-02-20 05:11:02 +0100

    added basic loader when component or page is not fully loaded

---
+++ Frontend/src/Loader/Loader.js
@@ -0,0 +1,13 @@
+function Loader() {
+  return (
+    <>
+      <div className='absolute flex w-full h-screen z-50'>
+        <div className='m-auto h-auto'>
+          <div className='border-gray-300 h-24 w-24 animate-spin rounded-full border-8 border-t-black' />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
