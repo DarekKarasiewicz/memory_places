ea0a611881aa8549b42d15371d29492fcb514650
CampoNover16 <s.mackiewicz00@gmail.com>
2024-03-01 20:43:03 +0100

    rewrited navbar for client needs + added footer

---
+++ Frontend/src/Footer/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer className='absolute bottom-0 w-full h-5 bg-slate-600 text-center text-white text-sm'>
+        Copyright © 2024 fundacja Miejsca Pamięci
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
