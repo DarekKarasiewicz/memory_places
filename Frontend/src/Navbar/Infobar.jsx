9450d4f026fefe078df0f097ca2da8cfd5271b2a
Mikolajkusinski <mikolajk558@gmail.com>
2024-03-09 10:51:59 +0100

    Added translation to infobar

--- Frontend/src/Navbar/Infobar.jsx
+++ Frontend/src/Navbar/Infobar.jsx
@@ -1,7 +1,10 @@
+import { useTranslation } from 'react-i18next';
+const { t } = useTranslation();
+
 const Infobar = () => {
   return (
     <div className='absolute bg-slate-400 p-3 top-5 left-1/2 transform -translate-x-1/2 rounded-lg '>
-      Kliknij na mapę aby wybrać lokację
+      {t('navbar.click_to_select')}
     </div>
   );
 };
