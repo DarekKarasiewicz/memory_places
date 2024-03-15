ae917a87bc6eaea4bd97b9218593bc42977fbffc
CampoNover16 <s.mackiewicz00@gmail.com>
2024-03-15 01:19:45 +0100

    added ContentNavbar, rewrited LanguageSwitcher

---
+++ Frontend/src/AdminDashboard/ContentNavbar/AdminDropdown/AdminDropdown.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import AdminDropdownItem from './AdminDropdownItem/AdminDropdownItem';
+
+function AdminDropdown() {
+  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { t } = useTranslation();
+
+  const menuItems = [
+    { icon: 'notification', name: t('user.notifications') },
+    { icon: 'pin', name: t('user.your_memory_places') },
+    { icon: 'settings', name: t('user.settings') },
+    { icon: 'help', name: t('user.help') },
+    { icon: 'contact', name: t('user.contact') },
+    { icon: 'logout', name: t('user.logout') },
+  ];
+
+  const parentItem = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.15,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const childItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  const handleClick = (event) => {
+    setIsActive((current) => !current);
+    setIsOpen((current) => !current);
+  };
+
+  return (
+    <>
+      <div className='pl-4'>
+        <div className='flex items-center cursor-pointer' onClick={handleClick}>
+          <div className='rounded-full h-10 w-10 flex justify-center items-center bg-slate-300 shadow-lg'>
+            <img src='./assets/user_icon.svg' alt='user_icon' className='h-6 w-6'></img>
+          </div>
+          {isOpen ? (
+            <img className='h-6 w-6' src='./assets/arrow_up_icon.svg' alt='up arrow icon'></img>
+          ) : (
+            <img className='h-6 w-6' src='./assets/arrow_down_icon.svg' alt='down arrow icon'></img>
+          )}
+        </div>
+        {isActive && (
+          <motion.ul
+            className='bg-slate-300 flex flex-col gap-2 mt-2 absolute top-16 right-2 w-52 p-4 z-10'
+            variants={parentItem}
+            initial='hidden'
+            animate='visible'
+          >
+            <li className='capitalize text-xl'>Username</li>
+            <li className='uppercase text-sm'>Administrator</li>
+
+            {menuItems.map((item, index) => (
+              <motion.li key={index} className='childItem' variants={childItem}>
+                {index === menuItems.length - 1 && <hr className='mb-2' />}
+                <AdminDropdownItem
+                  index={index}
+                  icon={item.icon}
+                  name={item.name}
+                  onClick={item.func}
+                />
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default AdminDropdown;
--- Frontend/src/LanguageSwitcher/LanguageSwitcher.js
+++ Frontend/src/LanguageSwitcher/LanguageSwitcher.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
 import i18n from '../i18n';
 
-function LanguageSwitcher() {
+function LanguageSwitcher(props) {
   const [language, setLanguage] = useState(i18n.language);
   const [isOpen, setIsOpen] = useState(false);
   const { t } = useTranslation();
@@ -23,11 +23,19 @@ function LanguageSwitcher() {
 
   return (
     <>
-      <div className='absolute right-0 top-20 my-auto'>
+      <div
+        className={`${
+          props.variant !== 'admin_dashboard' ? 'absolute right-0 top-20 my-auto' : 'relative'
+        }`}
+      >
         <div className='relative inline-block'>
           <button
             type='button'
-            className='inline-flex gap-1 justify-center items-center w-full rounded-l-lg border border-gray-600 bg-slate-300 pl-3 pr-1 py-2 active:bg-slate-50'
+            className={`${
+              props.variant !== 'admin_dashboard'
+                ? 'rounded-l-lg border border-gray-600 bg-slate-300 pl-3 pr-1 py-2 active:bg-slate-50'
+                : ''
+            } inline-flex gap-1 justify-center items-center w-full`}
             aria-haspopup='true'
             aria-expanded='true'
             onClick={() => setIsOpen(!isOpen)}
@@ -49,7 +57,13 @@ function LanguageSwitcher() {
           </button>
 
           {isOpen && (
-            <div className='absolute right-0 mt-2 w-24 rounded-l-lg shadow-lg bg-slate-600'>
+            <div
+              className={`${
+                props.variant !== 'admin_dashboard'
+                  ? 'right-0 rounded-l-lg bg-slate-600'
+                  : 'left-1/2 -translate-x-1/2 bg-white flex justify-center items-center'
+              } absolute mt-2 w-24 shadow-lg`}
+            >
               <div
                 className='py-1 origin-top'
                 role='menu'
@@ -60,7 +74,11 @@ function LanguageSwitcher() {
                   <motion.div
                     key={option.value}
                     onClick={() => handleLanguageChange(option.value)}
-                    className='block px-4 py-2 text-sm text-center text-white hover:bg-slate-400 hover:text-red-500 hover:font-bold cursor-pointer'
+                    className={`${
+                      props.variant !== 'admin_dashboard'
+                        ? 'text-white hover:bg-slate-400 hover:text-red-500'
+                        : 'hover:bg-slate-200 hover:text-cyan-600'
+                    } block hover:font-bold px-4 py-2 text-sm text-center cursor-pointer`}
                     role='menuitem'
                   >
                     <img src={option.image} alt={option.alt} className='drop-shadow-md' />
