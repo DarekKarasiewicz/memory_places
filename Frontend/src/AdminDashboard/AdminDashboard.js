2137482b99fa557c6d2b77edf9be8982c87698e9
CampoNover16 <s.mackiewicz00@gmail.com>
2024-03-15 01:19:45 +0100

    added ContentNavbar, rewrited LanguageSwitcher

--- Frontend/src/AdminDashboard/AdminDashboard.js
+++ Frontend/src/AdminDashboard/AdminDashboard.js
@@ -1,18 +1,22 @@
+import { Suspense } from 'react';
 import AdminMenu from './AdminMenu/AdminMenu.js';
 import ContentNavbar from './ContentNavbar/ContentNavbar.js';
 import Content from './Content/Content.js';
+import Loader from '../Loader/Loader.js';
 
 function AdminDashboard() {
   return (
-    <div className='w-screen h-screen relative flex'>
-      <div className='w-56 p-4 flex justify-center'>
-        <AdminMenu />
+    <Suspense fallback={<Loader />}>
+      <div className='w-screen h-screen relative flex'>
+        <div className='w-64 p-4 flex justify-center shadow-lg'>
+          <AdminMenu />
+        </div>
+        <div className='w-full flex flex-col'>
+          <ContentNavbar />
+          <Content />
+        </div>
       </div>
-      <div className='w-auto flex flex-col'>
-        <ContentNavbar />
-        <Content />
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
--- Frontend/src/AdminDashboard/ContentNavbar/ContentNavbar.js
+++ Frontend/src/AdminDashboard/ContentNavbar/ContentNavbar.js
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import LanguageSwitcher from '../../LanguageSwitcher/LanguageSwitcher';
+import AdminDropdown from './AdminDropdown/AdminDropdown';
+import i18n from '../../i18n';
+import 'moment/locale/pl';
+import 'moment/locale/ru';
+import 'moment/locale/de';
+
 function ContentNavbar() {
+  const [lang, setLang] = useState(i18n.language);
+  const [date, setDate] = useState(false);
+
+  useEffect(() => {
+    moment.locale(lang);
+    const currentDate = moment();
+    const dateString = currentDate.format('dddd D MMMM YYYY');
+
+    setDate(dateString);
+  }, [lang]);
+
+  useEffect(() => {
+    const updateLanguage = () => {
+      setLang(i18n.language);
+    };
+
+    i18n.on('languageChanged', updateLanguage);
+  }, []);
+
   return (
     <>
-      <div>TUTAJ</div>
+      <div className='flex justify-between w-full p-4 shadow-lg'>
+        <div className='text-3xl ml-4'>Welcome, user!</div>
+        <div className='flex'>
+          <div className='p-2 pr-4 border-r-2'>
+            <div className='flex justify-end items-center gap-8'>
+              <span className='capitalize'>{date}</span>
+              <LanguageSwitcher variant='admin_dashboard' />
+            </div>
+          </div>
+          <AdminDropdown />
+        </div>
+      </div>
     </>
   );
 }
