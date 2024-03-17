9e65796bd720d39c94b88ebc3af2e08386d5ef67
CampoNover16 <s.mackiewicz00@gmail.com>
2024-03-17 02:02:30 +0100

    added user management section with table features and test data(without actions functionalities), fixed height and overflow component issues, added statistics component, minor fixes

--- Frontend/src/AdminDashboard/Content/Charts/AdminTileStat.js
+++ Frontend/src/AdminDashboard/Content/Charts/AdminTileStat.js
@@ -5,11 +5,14 @@ function AdminTileStat(props) {
     <>
       <div className='flex flex-col justify-between shadow rounded-lg h-36 px-6 py-6 bg-white'>
         <span className='flex justify-start items-center gap-2 text-2xl leading-6'>
-          <img
-            src={`./assets/admin/${props.icon}_icon.svg`}
-            alt={`${props.icon}_icon`}
-            className='h-7 w-7'
-          ></img>
+          {props.icon && (
+            <img
+              src={`./assets/admin/${props.icon}_icon.svg`}
+              alt={`${props.icon}_icon`}
+              className='h-7 w-7'
+            ></img>
+          )}
+
           <span className=''>{props.title}</span>
         </span>
         <div className='flex justify-between mx-4'>
@@ -19,11 +22,13 @@ function AdminTileStat(props) {
               props.trend === 'up' ? 'bg-green-200 text-green-600' : 'bg-red-200 text-red-600'
             }`}
           >
-            <img
-              src={`./assets/admin/trending_${props.trend}_icon.svg`}
-              alt={`trending_${props.trend}_icon`}
-              className='h-4 w-4'
-            ></img>
+            {props.trend && (
+              <img
+                src={`./assets/admin/trending_${props.trend}_icon.svg`}
+                alt={`trending_${props.trend}_icon`}
+                className='h-4 w-4'
+              ></img>
+            )}
             <span>{props.percentage}%</span>
           </span>
         </div>
