659f769dbfe697f64236e732285a8212177aeaa6
CampoNover16 <s.mackiewicz00@gmail.com>
2024-03-20 01:40:20 +0100

    created place management section with backend data

--- Frontend/src/AdminDashboard/Content/Content.js
+++ Frontend/src/AdminDashboard/Content/Content.js
@@ -1,12 +1,14 @@
 import StatisticsSection from './ContentSections/StatisticsSection';
 import UserManagementSection from './ContentSections/UserManagementSection';
+import PlaceManagementSection from './ContentSections/PlaceManagamentSection';
 
 function Content() {
   return (
     <>
       <div className='px-10 py-8 bg-slate-100 flex flex-col gap-6 h-full'>
         {/* <StatisticsSection /> */}
-        <UserManagementSection />
+        {/* <UserManagementSection /> */}
+        <PlaceManagementSection />
       </div>
     </>
   );
