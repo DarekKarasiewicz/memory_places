9a698878669fc246d9867b0be6c7b5b4cbada180
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2024-04-16 20:02:12 +0200

    Models refactor in memo_places_forum

--- Backend/memo_places_forum/models.py
+++ Backend/memo_places_forum/models.py
@@ -1,26 +1,26 @@
 from django.db import models
+from memo_places.models import User
 from django.conf import settings
 # Create your models here.
 class Subforum(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    # Add other subreddit-related fields as needed
+    #Should be owner of subforum ?
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     subforum = models.ForeignKey(Subforum, on_delete=models.CASCADE,null=True)
-    like = models.IntegerField(null=True)
-    dislike = models.IntegerField(null=True)
-    # Add other post-related fields as needed
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
 
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    like = models.IntegerField(null=True)
-    dislike = models.IntegerField(null=True)
-    # Add other comment-related fields as needed
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
